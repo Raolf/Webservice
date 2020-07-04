03513b7e9840c9675161f2e02e74edd78c5a5a33
Raolf <42831107+Raolf@users.noreply.github.com>
2020-07-04 12:56:16 +0200

    Added Composite Keys

--- DataWebservice/DataWebservice/Models/Data.cs
+++ DataWebservice/DataWebservice/Models/Data.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataWebservice.Models
 {
     public class Data
     {
+        [Key, Column(Order = 0)]
         public int sensorID { get; set; }
         public Sensor sensor { get; set; }
+        [Key, Column(Order = 1)]
         public DateTime timestamp { get; set; }
         public int humidity { get; set; }
         public int CO2 { get; set; }
