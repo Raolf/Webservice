a874511ff6eba47d1b0c0b41be176896ec3a77f8
Raolf <42831107+Raolf@users.noreply.github.com>
2020-07-09 12:23:32 +0200

    LoriotDTO implemented

--- DataWebservice/DataWebservice/Data/LoriotDTO.cs
+++ DataWebservice/DataWebservice/Data/LoriotDTO.cs
@@ -7,5 +7,20 @@ namespace DataWebservice.Data
 {
     public class LoriotDTO
     {
+        public string cmd { get; set; }
+        public ulong seqno { get; set; }
+        public string EUI { get; set; }
+        public ulong ts { get; set; }
+        public int fcnt { get; set; }
+        public int port { get; set; }
+        public ulong freq { get; set; }
+        public int rssi { get; set; }
+        public float snr { get; set; }
+        public double toa { get; set; }
+        public string dr { get; set; }
+        public bool ack { get; set; }
+        public int bat { get; set; }
+        public bool offline { get; set; }
+        public string data { get; set; }
     }
 }
