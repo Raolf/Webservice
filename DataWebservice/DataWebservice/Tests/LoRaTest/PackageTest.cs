407d60029b549caf74057718a7a5c8aac3cb6ddb
Raolf <42831107+Raolf@users.noreply.github.com>
2020-07-11 01:57:55 +0200

    Added Tests & Changed some model types

---
+++ DataWebservice/DataWebservice/Tests/LoRaTest/PackageTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataWebservice.Tests.LoRaTest
+{
+    public class PackageTest
+    {
+    }
+}
