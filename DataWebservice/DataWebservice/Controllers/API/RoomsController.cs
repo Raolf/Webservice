31871c2e47ac1591643960284eaca112d3e29982
Raolf <42831107+Raolf@users.noreply.github.com>
2020-07-20 18:20:43 +0200

    RoomsAPI using Room

--- DataWebservice/DataWebservice/Controllers/API/RoomsController.cs
+++ DataWebservice/DataWebservice/Controllers/API/RoomsController.cs
@@ -25,15 +25,16 @@ namespace DataWebservice.Controllers.API
 
         // GET: api/Rooms
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<RoomDTO>>> GetRoom()
+        public async Task<ActionResult<IEnumerable<Room>>> GetRoom()
         {
-            var rooms = await _context.Room.ToListAsync();
-            List<RoomDTO> roomDTOs = new List<RoomDTO>();
+            return await _context.Room.ToListAsync();
+            //var rooms = await _context.Room.ToListAsync();
+            /*List<RoomDTO> roomDTOs = new List<RoomDTO>();
             foreach (Room room in rooms)
             {
                 roomDTOs.Add(room.ToDTO());
             }
-            return roomDTOs;
+            return roomDTOs;*/
         }
 
         // GET: api/Rooms/5
